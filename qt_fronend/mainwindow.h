504fb2e97c3458175a2f1c7fe359a524ba2c6f31
t0vit00 <tiiamariaviitanen@gmail.com>
2021-03-24 18:25:33 +0200

    allpersons started

--- qt_fronend/mainwindow.h
+++ qt_fronend/mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtNetwork>
+#include <QNetworkAccessManager>
+#include <QJsonDocument>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
